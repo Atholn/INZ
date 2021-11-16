bc5e9790d575c82905ec7ed4e299d4406e12660a
Atholn <mz3pr0gr4mmm3r@gmail.com>
2021-11-16 20:38:06 +0100

    INZ-82 Choose Map panel and lad name of maps

--- Assets/ChoseMapManager.cs
+++ Assets/ChoseMapManager.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ChoseMapManager : MonoBehaviour
 {
+    public GameObject ChooseMapPanel;
+    Dropdown MapsDropdown;
+    public GameObject PlaySettingsPanel;
+    public GameObject MapInfoPanel;
+    public GameObject ActionPanel;
+
+    FileMapSystem fileMapSystem;
+
+    private void Start()
+    {
+        fileMapSystem = new FileMapSystem() { FolderName = "Sirmish" };
+
+        MapsDropdown =  ChooseMapPanel.GetComponentInChildren<Dropdown>();
+
+        MapsDropdown.options.Clear();
+        MapsDropdown.AddOptions(fileMapSystem.GetNamesMaps(fileMapSystem.FolderName));
+    }
+
+    public void ChooseMap()
+    {
+
+    }
+
     public void PlayButton()
     {
         SceneManager.LoadScene("Sirmish");
