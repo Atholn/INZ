c7345877627be6e4f8c6b304db83b11a0db1f6a7
Atholn <mz3pr0gr4mmm3r@gmail.com>
2022-02-21 23:43:29 +0100

    INZ-255 Refactored MainMenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //public void Play()
-    //{
-    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    //}
-
     public void Sirmish()
     {
         SceneManager.LoadScene("SirmishChose");
