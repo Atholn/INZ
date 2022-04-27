b8aa80e4320172046faffd140a3965fc62529775
Atholn <mz3pr0gr4mmm3r@gmail.com>
2022-04-27 21:58:27 +0200

    INZ-264 Add logic to next previous map

--- Assets/Scripts/Campaign/CampaignCameraControll.cs
+++ Assets/Scripts/Campaign/CampaignCameraControll.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class CampaignCameraControll : MonoBehaviour
 {
+    private Vector3 shiftVector = new Vector3(0, 0, -4);
 
+    internal void GoToCampaignPoint(Vector2 newTargetPos)
+    {
+        transform.position = new Vector3(newTargetPos.x, transform.position.y, newTargetPos.y) + shiftVector;
+    }
 }
--- Assets/Scripts/Campaign/CampaignManager.cs
+++ Assets/Scripts/Campaign/CampaignManager.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CampaignManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private List<CampaignMapPoint> campaignMapPoints;
+    private CampaignCameraControll campaignCameraControll;
+    private int actualTarget = 0;
+    private int availableTarget;
+
     void Start()
     {
-        
+        campaignCameraControll = GameObject.FindObjectOfType<CampaignCameraControll>();
+        campaignMapPoints = GameObject.FindObjectsOfType<CampaignMapPoint>().OrderBy(cmp => cmp.name).ToList();
     }
 
-    // Update is called once per frame
+    private void InitializePlayerCampaignSettings()
+    {
+        //todo
+        //read files , or dont files create
+
+    }
+
     void Update()
     {
         
@@ -18,12 +30,31 @@ public class CampaignManager : MonoBehaviour
 
     internal void NextCampaignMap()
     {
+        if (actualTarget + 1 >= campaignMapPoints.Count)
+        {
+            return;
+        }
 
+        actualTarget++;
+        SetNewTargetPos(campaignMapPoints[actualTarget]);
     }
 
     internal void PreviousCampaignMap()
     {
+        if (actualTarget - 1 < 0)
+        {
+            return;
+        }
 
+        actualTarget--;
+        SetNewTargetPos(campaignMapPoints[actualTarget]);
+    }
+
+    private void SetNewTargetPos(CampaignMapPoint campaignMapPoint)
+    {
+        Transform transform = campaignMapPoint.GetComponent<Transform>();
+        Vector2 newTargetPos = new Vector2(transform.position.x, transform.position.z);
+        campaignCameraControll.GoToCampaignPoint(newTargetPos);
     }
 
 }
