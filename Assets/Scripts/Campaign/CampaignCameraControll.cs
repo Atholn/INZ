7dab2239df17ae2d4a62b87d36bff6b4fee35491
Atholn <mz3pr0gr4mmm3r@gmail.com>
2022-04-27 23:01:08 +0200

    INZ-264 Camera controll movement

--- Assets/Scripts/Campaign/CampaignCameraControll.cs
+++ Assets/Scripts/Campaign/CampaignCameraControll.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class CampaignCameraControll : MonoBehaviour
 {
     private Vector3 shiftVector = new Vector3(0, 0, -4);
+    private Vector3 actualPosTarget;
+
+    private void Start()
+    {
+        actualPosTarget = transform.position;
+    }
+
+    private void Update()
+    {
+        if(transform.position != actualPosTarget)
+        {
+            transform.position = Vector3.Lerp(transform.position, actualPosTarget, 2.0f * Time.deltaTime);
+        }
+
+    }
 
     internal void GoToCampaignPoint(Vector2 newTargetPos)
     {
-        transform.position = new Vector3(newTargetPos.x, transform.position.y, newTargetPos.y) + shiftVector;
+        actualPosTarget = new Vector3(newTargetPos.x, transform.position.y, newTargetPos.y) + shiftVector;
     }
 }
